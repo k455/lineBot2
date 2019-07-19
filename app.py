a6aaf31dda84503614a8888f3968578221ea7347
k455 <51470935+k455@users.noreply.github.com>
2019-07-19 11:36:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,11 +23,11 @@ def callback():
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
     #sendText(user,userText)
-    if   ( userText == 'สวัสดี') :
+    if (userText == 'สวัสดี') :
        sendText(user,'Hello')
-    elif ( userText== 'ไอ้บ้า') :
+    elif (userText== 'ไอ้บ้า') :
        sendText(user,'เทอซิบ้า')
-    elif ( userText == 'เหมือนหล่อ') :
+    elif (userText == 'เหมือนหล่อ') :
        sendText(user,'ไรเมิง')
     return '',200
 
